8ee778b677ec805fa16340b0eb9ab33e8632c0ae
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-09-10 17:36:29 +0300

    создал массив с аватарами

--- js/map.js
+++ js/map.js
@@ -1,2 +1,14 @@
-'use strict';
+ADVER_COUNT = 8;
+
+var similarAdvertisments = {};
+
+var avatar = [];
+
+for (var i = 0; i < ADVER_COUNT; i++) {
+  avatar[i] = 'img/avatars/user0' + i;
+}
+
+similarAdvertisments.autor = {avatar};
+
+console.log(similarAdvertisments);
 
