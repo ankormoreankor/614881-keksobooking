3a074be9d3ea5bc6ec34e9b6b6d2e214f68c8c77
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-09-13 16:47:15 +0300

    написал фунукции нахождения случайного значения массива и случайного числа от минимума до максимума, написал функции расчёта некоторых из свойств объекта

--- js/map.js
+++ js/map.js
@@ -1,65 +1,85 @@
-// var AVATAR_NUMBER_MIN = 1;
-var AVATAR_NUMBER_MAX = 8;
 var LOWEST_PRICE = 1000;
 var HIGHEST_PRICE = 1000000;
-var ROOMS_MINIMUM = 1;
-var ROOMS_MAXIMUM = 5;
+var ROOMS_MIN = 1;
+var ROOMS_MAX = 5;
 var BEDS_IN_ROOM = 2;
 var PHOTOS_NUMBER_MAX = 3;
-var LOCATION_Y_MIN = 130;
-var LOCATION_Y_MAX = 630;
-
-var landlordAvatars = [];
-
-for (var i = 0; i < AVATAR_NUMBER_MAX; i++) {
-  var avatarPath = 'img/avatars/user'
-
-  if (i < 9) {
-    landlordAvatars[i] = avatarPath + '0' + (i + 1) + '.png';
-  } else {
-    landlordAvatars[i] = avatarPath + (i + 1) + '.png';
-  }
-}
-
-var timestamps = ['12:00', '13:00', '14:00'];
-
-var guests_minimum = ROOMS_MINIMUM * BEDS_IN_ROOM;
-var guests_maximum = ROOMS_MAXIMUM * BEDS_IN_ROOM;
+var MAP_WIDTH = 980;
+var MAP_HEIGHT = 750;
+var MAP_INDENT = 130;
+var AVATAR_PATH = 'img/avatars/user';
+var AVATAR_EXTENTION = '.png';
+var PHOTO_PATH = 'http://o0.github.io/assets/images/tokyo/hotel';
+var PHOTO_EXTENTION = '.jpg';
 
 var advertismentTitles = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
 
 var houseTypes = ['palace', 'flat', 'house', 'bungalo'];
+
+var timestamps = ['12:00', '13:00', '14:00'];
 
 var checkinTimes = timestamps;
 var checkoutTimes = timestamps;
 
 var houseFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
-var housePhotos = [];
-
-for (var i = 0; i < PHOTOS_NUMBER_MAX; i++) {
-  var photoPath = 'http://o0.github.io/assets/images/tokyo/hotel';
-
-  housePhotos[i] = photoPath + (i + 1) + '.jpg';
-}
-
-
 var createSimilarAdvertisement = function (avatarNumber) {
   var similarAdvertisement = {};
 
-  var avatarPath = 'img/avatars/user'
+  var avatar = 0;
+  var title = 0;
+  var adress = 0;
+  var price = 0;
+  var type = 0;
+  var rooms = 0;
+  var guests = 0;
+  var checkin = 0;
+  var checkout = 0;
+  var features = [];
+  var description = '';
+  var photos = [];
+  var x = 0;
+  var y = 0;
 
-  if (avatarNumber < 10) {
-    avatar = avatarPath + '0' + avatarNumber + '.png';
-  } else {
-    avatar = avatarPath + avatarNumber + '.png';
+  var randomValue = function (arr) {
+    return value = Math.round(Math.random() * (arr.length - 1));
+  };
+
+  var randomFromTwo = function (min, max) {
+    return number = Math.round(min + Math.random() * (max - min));
   }
 
+  if (avatarNumber < 10) {
+    avatar = AVATAR_PATH + '0' + avatarNumber + AVATAR_EXTENTION;
+  } else {
+    avatar = AVATAR_PATH + avatarNumber + AVATAR_EXTENTION;
+  }
+
+  title = advertismentTitles[randomValue(advertismentTitles)];
+
+  var locationXMin = MAP_INDENT;
+  var locationXMax = MAP_WIDTH - MAP_INDENT;
+  var locationYMin = MAP_INDENT;
+  var locationYMax = MAP_HEIGHT - MAP_INDENT;
+
+  adress = randomFromTwo(locationXMin, locationXMax) + ', ' + randomFromTwo(locationYMin, locationYMax);
+
+  price = randomFromTwo(LOWEST_PRICE, HIGHEST_PRICE);
+
+  type = houseTypes[randomValue(houseTypes)];
+
+  rooms = randomFromTwo(ROOMS_MIN, ROOMS_MAX);
+
+  var guests_minimum = rooms * BEDS_IN_ROOM;
+  var guests_maximum = rooms * BEDS_IN_ROOM;
+
+  guests = randomFromTwo(guests_minimum, guests_maximum);
+
   similarAdvertisement.autor = {avatar};
-  // similarAdvertisement = autor;
+  similarAdvertisement.offer = {title, adress, price, type, rooms, guests};
 
 
-  console.log(similarAdvertisement.autor);
+  console.log(similarAdvertisement.offer);
 
 };
 
