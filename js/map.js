1d5fc628b54d859e892101ad2fa93ee5c031d013
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-09-13 19:21:38 +0300

    дописал функцию создания объекта похожих объявлений

--- js/map.js
+++ js/map.js
@@ -38,12 +38,16 @@ var createSimilarAdvertisement = function (avatarNumber) {
   var x = 0;
   var y = 0;
 
-  var randomValue = function (arr) {
-    return value = Math.round(Math.random() * (arr.length - 1));
+  var getRandomValue = function (arr) {
+    return Math.round(Math.random() * (arr.length - 1));
   };
 
-  var randomFromTwo = function (min, max) {
-    return number = Math.round(min + Math.random() * (max - min));
+  var getRandomFromTwo = function (min, max) {
+    return Math.round(min + Math.random() * (max - min));
+  }
+
+  var getRandomArrayItem = function (arr) {
+    return arr.splice(getRandomValue(arr), 1);
   }
 
   if (avatarNumber < 10) {
@@ -52,47 +56,52 @@ var createSimilarAdvertisement = function (avatarNumber) {
     avatar = AVATAR_PATH + avatarNumber + AVATAR_EXTENTION;
   }
 
-  title = advertismentTitles[randomValue(advertismentTitles)];
+  title = advertismentTitles[getRandomValue(advertismentTitles)];
 
   var locationXMin = MAP_INDENT;
   var locationXMax = MAP_WIDTH - MAP_INDENT;
   var locationYMin = MAP_INDENT;
   var locationYMax = MAP_HEIGHT - MAP_INDENT;
 
-  adress = randomFromTwo(locationXMin, locationXMax) + ', ' + randomFromTwo(locationYMin, locationYMax);
+  adress = getRandomFromTwo(locationXMin, locationXMax) + ', ' + getRandomFromTwo(locationYMin, locationYMax);
 
-  price = randomFromTwo(LOWEST_PRICE, HIGHEST_PRICE);
+  price = getRandomFromTwo(LOWEST_PRICE, HIGHEST_PRICE);
 
-  type = houseTypes[randomValue(houseTypes)];
+  type = houseTypes[getRandomValue(houseTypes)];
 
-  rooms = randomFromTwo(ROOMS_MIN, ROOMS_MAX);
+  rooms = getRandomFromTwo(ROOMS_MIN, ROOMS_MAX);
 
   var guests_minimum = rooms * BEDS_IN_ROOM;
   var guests_maximum = rooms * BEDS_IN_ROOM;
 
-  guests = randomFromTwo(guests_minimum, guests_maximum);
+  guests = getRandomFromTwo(guests_minimum, guests_maximum);
 
   var checkinTimes = timestamps;
   var checkoutTimes = timestamps;
 
-  checkin = checkinTimes[randomValue(checkinTimes)];
-  checkout = checkoutTimes[randomValue(checkoutTimes)];
+  checkin = checkinTimes[getRandomValue(checkinTimes)];
+  checkout = checkoutTimes[getRandomValue(checkoutTimes)];
 
-  var newArray = houseFeatures.slice(0, houseFeatures.length);
+  var newFeaturesArr = houseFeatures.slice(0, houseFeatures.length);
 
-  for (var i = 0; i < randomValue(newArray); i++) {
-    features = features.concat(newArray.splice(randomValue(newArray), 1));
+  for (var i = 0; i < getRandomValue(newFeaturesArr); i++) {
+    features = features.concat(getRandomArrayItem(newFeaturesArr));
   }
 
+  var photosForSort = [];
+
+  for (var i = 0; i < PHOTOS_NUMBER_MAX; i++) {
+    photosForSort[i] = PHOTO_PATH + (i + 1) + PHOTO_EXTENTION;
+  }
+
+  for (var i = 0; i < PHOTOS_NUMBER_MAX; i++) {
+    photos = photos.concat(getRandomArrayItem(photosForSort));
+  }
+
+  x = getRandomFromTwo(locationXMin, locationXMax);
+  y = getRandomFromTwo(locationYMin, locationYMax);
+
   similarAdvertisement.autor = {avatar};
-  similarAdvertisement.offer = {title, adress, price, type, rooms, guests, checkin, checkout, features, description};
-
-
-  console.log(similarAdvertisement.offer.features);
-
+  similarAdvertisement.offer = {title, adress, price, type, rooms, guests, checkin, checkout, features, description, photos};
+  similarAdvertisement.location = {x, y};
 };
-
-createSimilarAdvertisement(1);
-
-
-// console.log();
