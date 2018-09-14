6baeba24e71965d687520d9bddec0af0e3b312ae
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-09-14 18:07:13 +0300

    переписал код в функции, написал вызовы, всё работает, ура товарищи!

--- js/map.js
+++ js/map.js
@@ -21,6 +21,8 @@ var timestamps = ['12:00', '13:00', '14:00'];
 
 var houseFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
+
+// Создаю объект описанием жилища
 var createSimilarAdvertisement = function (avatarNumber) {
   var similarAdvertisement = {};
 
@@ -85,7 +87,7 @@ var createSimilarAdvertisement = function (avatarNumber) {
 
   var newFeaturesArr = houseFeatures.slice(0, houseFeatures.length);
 
-  for (var i = 0; i < getRandomValue(newFeaturesArr); i++) {
+  for (var i = 0; i <= getRandomValue(newFeaturesArr); i++) {
     features = features.concat(getRandomArrayItem(newFeaturesArr));
   }
 
@@ -109,10 +111,135 @@ var createSimilarAdvertisement = function (avatarNumber) {
   return similarAdvertisement;
 };
 
+// "Включаю" карту
+var activateMap = function (booleen) {
+  return booleen = true ?
+        document.querySelector('.map').classList.remove('map--faded') :
+        document.querySelector('.map').classList.add('map--faded');
+}
+
+// Копирую шаблон меток, множу его, добавляю аватары
+var createPins = function (pinsCount, container) {
+  var pinTemplate = document
+      .querySelector('#pin')
+      .content
+      .querySelector('.map__pin');
+
+  for (var i = 0; i < pinsCount; i++) {
+    var pin = pinTemplate.cloneNode(true);
+    pin.style.left = advertisements[i].location.x + 'px';
+    pin.style.top = advertisements[i].location.y + 'px';
+    pin.children[0].src = advertisements[i].autor.avatar;
+    pin.children[0].alt = advertisements[i].offer.title;
+
+    container.appendChild(pin);
+  }
+
+  return container;
+}
+
+// Добавляю метки на карту
+var addPins = function (parent ,fragment) {
+  return parent.appendChild(fragment);
+}
+
+var createPopup = function (landlordNumber) {
+  // Копирую шаблон попапа, наполняю информацией
+  var mapFiltersConteiner = document.querySelector('.map__filters-container');
+  var cardTemplate = document
+      .querySelector('#card')
+      .content
+      .querySelector('.map__card');
+
+  var popupCard = cardTemplate.cloneNode(true);
+
+  popupCard.querySelector('.popup__title').textContent = advertisements[landlordNumber].offer.title;
+  popupCard.querySelector('.popup__text--address').textContent = advertisements[landlordNumber].offer.adress;
+  popupCard.querySelector('.popup__text--price').textContent = advertisements[landlordNumber].offer.price + '₽/ночь';
+
+  // Заменяю англоязычное название жилища на рускоязычное
+  var houseTypesRus = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
+
+  for (var i = 0; i < houseTypes.length; i++) {
+    if (houseTypes[i] === advertisements[landlordNumber].offer.type) {
+      popupCard.querySelector('.popup__type').textContent = houseTypesRus[i];
+    }
+  }
+
+  popupCard.querySelector('.popup__text--capacity').textContent =
+    advertisements[landlordNumber].offer.rooms +
+    ' комнаты для ' + advertisements[landlordNumber].offer.guests + ' гостей';
+
+  popupCard.querySelector('.popup__text--time').textContent =
+    'Заезд после ' + advertisements[landlordNumber].offer.checkin +
+    ', выезд до ' + advertisements[landlordNumber].offer.checkout;
+
+  // Удаляю текущие элементы li, создаю свои, количеством равным длине массива features,
+  // добавляю такие же классы, как были, с поправкой на содержимое массива
+  var popupFeatures = popupCard.querySelector('.popup__features');
+  var liTemplates = popupCard.querySelectorAll('.popup__feature');
+
+  for (var i = 0; i < liTemplates.length; i++) {
+    popupFeatures.removeChild(popupFeatures.children[0]);
+  }
+
+  var popupFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < advertisements[landlordNumber].offer.features.length; i++) {
+    var popupFeatureLi = document.createElement('li');
+    popupFeatureLi.classList.add('popup__feature');
+    popupFeatureLi.classList.add('popup__feature--' + advertisements[landlordNumber].offer.features[i]);
+
+    popupFragment.appendChild(popupFeatureLi);
+  }
+
+  popupFeatures.appendChild(popupFragment);
+
+  popupCard.querySelector('.popup__description').textContent = advertisements[landlordNumber].offer.description;
+
+  // Клонирую объект img, заполняю src, записываю в фрагмент, удаляю исходный объект и присоединяю фрагмент
+  var popupPhoto = popupCard.querySelector('.popup__photo');
+  var photosFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < PHOTOS_NUMBER_MAX; i++) {
+    var popupPhotoImg = popupPhoto.cloneNode(true);
+    popupPhotoImg.src = advertisements[landlordNumber].offer.photos[i];
+
+    photosFragment.appendChild(popupPhotoImg);
+  }
+
+  var popupPhotos = popupCard.querySelector('.popup__photos');
+  popupPhotos.removeChild(popupPhoto);
+  popupCard.querySelector('.popup__photos').appendChild(photosFragment);
+
+  popupCard.querySelector('.popup__avatar').src = advertisements[landlordNumber].autor.avatar;
+
+
+  document.querySelector('.map').insertBefore(popupCard, mapFiltersConteiner);
+}
+
+// НАЧАЛО ПРОГРАММЫ
+
+// Создаю массив, заполняю объектами
 var advertisements = [];
 
-for (var i = 0; i < ADS_COUNT; i++) {
+for (var i = 1; i <= ADS_COUNT; i++) {
   advertisements = advertisements.concat(createSimilarAdvertisement(i));
 }
 
-console.log(advertisements[4].offer);
+activateMap(true);
+
+var mapPin = document.querySelector('.map__pins');
+var pinFragment = document.createDocumentFragment();
+
+createPins(ADS_COUNT, pinFragment);
+addPins(mapPin, pinFragment);
+createPopup(0);
+
+
+
+
+
+
+// console.log(advertisements[landlordNumber].offer.type);
+
