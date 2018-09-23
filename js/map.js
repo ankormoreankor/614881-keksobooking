9b9ea1cbaa71b0435f315209e3daa584dd102c1b
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-09-23 22:16:34 +0300

    порефакторил код

--- js/map.js
+++ js/map.js
@@ -92,27 +92,57 @@ var mixArr = function (array) {
   return newArr;
 };
 
-var compareAndReturn = function (arr_1, arr_2, condition) {
+var compareAndReturn = function (comparedArr, condition, returnedArr) {
 
-  for (var i = 0; i < arr_1.length; i++) {
-    if (arr_1[i] === condition) {
+  for (var i = 0; i < comparedArr.length; i++) {
+    if (comparedArr[i] === condition) {
       break;
     }
   }
 
-  return arr_2[i];
+  return returnedArr[i];
 };
 
-var createChild = function (tag, attribute, valueArr) {
+var createChild = function (tag, attribute, value) {
   var newElem = document.createElement(tag);
 
-  newElem.setAttribute(attribute, valueArr);
+  newElem.setAttribute(attribute, value);
 
   return newElem;
 };
 
 var addElem = function (parent, child) {
   return parent.appendChild(child);
+};
+
+var insertText = function (selector, content, parentBlock) {
+  var elem = parentBlock.querySelector(selector);
+
+  return elem.textContent = content;
+};
+
+var deleteChilds = function (selector, node, parentBlock) {
+  var childs = parentBlock.querySelectorAll(selector);
+
+  for (var i = 0; i < childs.length; i++) {
+    node.removeChild(node.children[0]);
+  }
+
+  return node;
+};
+
+var appendPhotos = function (parentBlock, photosArray, selector) {
+  var fragment = document.createDocumentFragment();
+  parentBlock.children[0].src = photosArray[0];
+
+  for (var i = 1; i < photosArray.length; i++) {
+    var img = parentBlock.querySelector(selector).cloneNode(true);
+    img.src = photosArray[i];
+
+    fragment.appendChild(img);
+  }
+
+  return parentBlock.appendChild(fragment);
 };
 
 var createSimilarAd = function (avatarNumber) {
@@ -147,53 +177,34 @@ var createSimilarAd = function (avatarNumber) {
   };
 };
 
-var activateMap = function (booleen) {
-  return booleen = true ?
+var activateMap = function (condition) {
+  return condition = true ?
         document.querySelector('.map').classList.remove('map--faded') :
         document.querySelector('.map').classList.add('map--faded');
 };
 
-var createPins = function (array, container) {
+var createPins = function (pinsArray) {
+  var fragment = document.createDocumentFragment();
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 0; i < pinsArray.length; i++) {
     var pin = pinTemplate.cloneNode(true);
-    pin.style.left = array[i].location.x + 'px';
-    pin.style.top = array[i].location.y + 'px';
-    pin.children[0].src = array[i].author.avatar;
-    pin.children[0].alt = array[i].offer.title;
+    pin.style.left = pinsArray[i].location.x + 'px';
+    pin.style.top = pinsArray[i].location.y + 'px';
+    pin.children[0].src = pinsArray[i].author.avatar;
+    pin.children[0].alt = pinsArray[i].offer.title;
 
-    container.appendChild(pin);
+    fragment.appendChild(pin);
   }
 
-  return container;
+  return fragment;
 };
 
-var mapFiltersContainer = document.querySelector('.map__filters-container');
-var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
-
 var createPopup = function (landlordNumber) {
+  var mapFiltersContainer = document.querySelector('.map__filters-container');
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
   var popupCard = cardTemplate.cloneNode(true);
-  var popupFeatures = popupCard.querySelector('.popup__features');
-  var popupPhotos = popupCard.querySelector('.popup__photos');
-
   var addsOffer = advertisements[landlordNumber].offer;
-
-  var insertText = function (selector, node) {
-    var parent = popupCard.querySelector(selector);
-
-    return parent.textContent = node;
-  };
-
-  var deleteChilds = function (selector, parent) {
-    var childs = popupCard.querySelectorAll(selector);
-
-    for (var i = 0; i < childs.length; i++) {
-      parent.removeChild(parent.children[0]);
-    }
-
-    return parent;
-  };
 
   var appendFeatures = function (parent) {
     var fragment = document.createDocumentFragment();
@@ -205,32 +216,20 @@ var createPopup = function (landlordNumber) {
     return parent.appendChild(fragment);
   };
 
-  var appendPhotos = function (parent) {
-    var fragment = document.createDocumentFragment();
-    popupPhotos.children[0].src = addsOffer.photos[0];
+  insertText('.popup__title', addsOffer.title, popupCard);
+  insertText('.popup__text--address', addsOffer.adress, popupCard);
+  insertText('.popup__text--price', addsOffer.price + '₽/ночь', popupCard);
+  insertText('.popup__type', compareAndReturn(houseTypes, addsOffer.type, houseTypesRus), popupCard);
+  insertText('.popup__text--capacity', addsOffer.rooms + ' комнаты для ' + addsOffer.guests + ' гостей', popupCard);
+  insertText('.popup__text--time', 'Заезд после ' + addsOffer.checkin + ', выезд до ' + addsOffer.checkout, popupCard);
+  insertText('.popup__description', addsOffer.description, popupCard);
 
-    for (var i = 1; i < housePhotos.length; i++) {
-      var popupPhotoImg = popupCard.querySelector('.popup__photo').cloneNode(true);
-      popupPhotoImg.src = addsOffer.photos[i];
-
-      fragment.appendChild(popupPhotoImg);
-    }
-
-    return parent.appendChild(fragment);
-  };
-
-  insertText('.popup__title', addsOffer.title);
-  insertText('.popup__text--address', addsOffer.adress);
-  insertText('.popup__text--price', addsOffer.price + '₽/ночь');
-  insertText('.popup__type', compareAndReturn(houseTypes, houseTypesRus, addsOffer.type));
-  insertText('.popup__text--capacity', addsOffer.rooms + ' комнаты для ' + addsOffer.guests + ' гостей');
-  insertText('.popup__text--time', 'Заезд после ' + addsOffer.checkin + ', выезд до ' + addsOffer.checkout);
-  insertText('.popup__description', addsOffer.description);
-
-  deleteChilds('.popup__feature', popupFeatures);
+  var popupFeatures = popupCard.querySelector('.popup__features');
+  deleteChilds('.popup__feature', popupFeatures, popupCard);
   appendFeatures(popupFeatures);
 
-  appendPhotos(popupPhotos);
+  var popupPhotos = popupCard.querySelector('.popup__photos');
+  appendPhotos(popupPhotos, addsOffer.photos, '.popup__photo');
 
   popupCard.querySelector('.popup__avatar').src = advertisements[landlordNumber].author.avatar;
 
@@ -251,8 +250,6 @@ createAdsArr(ADS_COUNT);
 activateMap(true);
 
 var mapPin = document.querySelector('.map__pins');
-var pinFragment = document.createDocumentFragment();
 
-createPins(advertisements, pinFragment);
-addElem(mapPin, pinFragment);
+addElem(mapPin, createPins(advertisements));
 createPopup(0);
