58a5c53add53593e6c84f501b59dcb535f820185
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-10-07 18:07:57 +0300

    вынес утилиты в util.js

--- js/map.js
+++ js/map.js
@@ -12,7 +12,6 @@ var MAP_RIGHT_INDENT = 40;
 var AVATAR_PATH = 'img/avatars/user0';
 var AVATAR_EXTENTION = '.png';
 var POINTER_ARROW_HEIGHT = 22;
-var ESC_KEYCODE = 27;
 
 var advertismentTitles = [
   'Большая уютная квартира',
@@ -63,81 +62,6 @@ var houseTypesRus = [
 var mapBlock = document.querySelector('.map');
 var mapWidth = mapBlock.offsetWidth;
 
-var getRandomValue = function (arr) {
-  return Math.round(Math.random() * (arr.length - 1));
-};
-
-var getRandomFromTwo = function (min, max) {
-  return Math.round(min + Math.random() * (max - min));
-};
-
-var getRandomArrItem = function (arr) {
-  arr = arr.splice(getRandomValue(arr), 1);
-  return arr[0];
-};
-
-var createRandomArr = function (array) {
-  var transitArr = array.slice(0, array.length);
-  var newArr = [];
-
-  for (var i = 0; i < getRandomValue(transitArr); i++) {
-    newArr = newArr.concat(getRandomArrItem(transitArr));
-  }
-
-  return newArr;
-};
-
-var mixArr = function (array) {
-  var transitArr = array.slice(0, array.length);
-  var newArr = [];
-
-  for (var i = 0; i < array.length; i++) {
-    newArr[i] = getRandomArrItem(transitArr);
-  }
-
-  return newArr;
-};
-
-var compareAndReturn = function (comparedArr, condition, returnedArr) {
-
-  for (var i = 0; i < comparedArr.length; i++) {
-    if (comparedArr[i] === condition) {
-      break;
-    }
-  }
-
-  return returnedArr[i];
-};
-
-var createChild = function (tag, attribute, value) {
-  var newElem = document.createElement(tag);
-
-  newElem.setAttribute(attribute, value);
-
-  return newElem;
-};
-
-var addElem = function (parent, child) {
-  return parent.appendChild(child);
-};
-
-var insertText = function (selector, content, parentBlock) {
-  var elem = parentBlock.querySelector(selector);
-  elem.textContent = content;
-
-  return elem;
-};
-
-var deleteChilds = function (selector, parentBlock) {
-  var childs = parentBlock.querySelectorAll(selector);
-
-  for (var i = 0; i < childs.length; i++) {
-    parentBlock.removeChild(childs[i]);
-  }
-
-  return parentBlock;
-};
-
 var appendPhotos = function (parentBlock, photosArray, selector) {
   var fragment = document.createDocumentFragment();
   parentBlock.children[0].src = photosArray[0];
@@ -156,7 +80,7 @@ var appendFeatures = function (parentBlock, featuresList) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < featuresList.length; i++) {
-    fragment.appendChild(createChild('li', 'class', 'popup__feature' + ' ' + 'popup__feature--' + featuresList[i]));
+    fragment.appendChild(window.util.createChild('li', 'class', 'popup__feature' + ' ' + 'popup__feature--' + featuresList[i]));
   }
 
   return parentBlock.appendChild(fragment);
@@ -164,8 +88,8 @@ var appendFeatures = function (parentBlock, featuresList) {
 
 var createSimilarAd = function (avatarNumber) {
   var location = {
-    x: getRandomFromTwo(MAP_MIN_LEFT, mapWidth - MAP_RIGHT_INDENT),
-    y: getRandomFromTwo(MAP_MIN_TOP, MAP_MAX_TOP)
+    x: window.util.getRandomFromTwo(MAP_MIN_LEFT, mapWidth - MAP_RIGHT_INDENT),
+    y: window.util.getRandomFromTwo(MAP_MIN_TOP, MAP_MAX_TOP)
   };
 
   return {
@@ -174,17 +98,17 @@ var createSimilarAd = function (avatarNumber) {
     },
 
     offer: {
-      title: advertismentTitles[getRandomValue(advertismentTitles)],
+      title: advertismentTitles[window.util.getRandomValue(advertismentTitles)],
       address: location.x + ' ' + location.y,
-      price: getRandomFromTwo(LOWEST_PRICE, HIGHEST_PRICE),
-      type: houseTypes[getRandomValue(houseTypes)],
-      rooms: getRandomFromTwo(ROOMS_MIN, ROOMS_MAX),
-      guests: getRandomFromTwo(ROOMS_MIN, ROOMS_MAX),
-      checkin: checkinCheckout[getRandomValue(checkinCheckout)],
-      checkout: checkinCheckout[getRandomValue(checkinCheckout)],
-      features: createRandomArr(houseFeatures),
+      price: window.util.getRandomFromTwo(LOWEST_PRICE, HIGHEST_PRICE),
+      type: houseTypes[window.util.getRandomValue(houseTypes)],
+      rooms: window.util.getRandomFromTwo(ROOMS_MIN, ROOMS_MAX),
+      guests: window.util.getRandomFromTwo(ROOMS_MIN, ROOMS_MAX),
+      checkin: checkinCheckout[window.util.getRandomValue(checkinCheckout)],
+      checkout: checkinCheckout[window.util.getRandomValue(checkinCheckout)],
+      features: window.util.createRandomArr(houseFeatures),
       description: '',
-      photos: mixArr(housePhotos)
+      photos: window.util.mixArr(housePhotos)
     },
 
     location: {
@@ -218,16 +142,16 @@ var createPopup = function (landlordNumber) {
   var addsOffer = advertisements[landlordNumber].offer;
 
 
-  insertText('.popup__title', addsOffer.title, popupCard);
-  insertText('.popup__text--address', addsOffer.adress, popupCard);
-  insertText('.popup__text--price', addsOffer.price + '₽/ночь', popupCard);
-  insertText('.popup__type', compareAndReturn(houseTypes, addsOffer.type, houseTypesRus), popupCard);
-  insertText('.popup__text--capacity', addsOffer.rooms + ' комнаты для ' + addsOffer.guests + ' гостей', popupCard);
-  insertText('.popup__text--time', 'Заезд после ' + addsOffer.checkin + ', выезд до ' + addsOffer.checkout, popupCard);
-  insertText('.popup__description', addsOffer.description, popupCard);
+  window.util.insertText('.popup__title', addsOffer.title, popupCard);
+  window.util.insertText('.popup__text--address', addsOffer.adress, popupCard);
+  window.util.insertText('.popup__text--price', addsOffer.price + '₽/ночь', popupCard);
+  window.util.insertText('.popup__type', window.util.compareAndReturn(houseTypes, addsOffer.type, houseTypesRus), popupCard);
+  window.util.insertText('.popup__text--capacity', addsOffer.rooms + ' комнаты для ' + addsOffer.guests + ' гостей', popupCard);
+  window.util.insertText('.popup__text--time', 'Заезд после ' + addsOffer.checkin + ', выезд до ' + addsOffer.checkout, popupCard);
+  window.util.insertText('.popup__description', addsOffer.description, popupCard);
 
   var popupFeatures = popupCard.querySelector('.popup__features');
-  appendFeatures(deleteChilds('.popup__feature', popupFeatures), addsOffer.features);
+  appendFeatures(window.util.deleteChilds('.popup__feature', popupFeatures), addsOffer.features);
 
   var popupPhotos = popupCard.querySelector('.popup__photos');
   appendPhotos(popupPhotos, addsOffer.photos, '.popup__photo');
@@ -253,33 +177,6 @@ var mapPinsBlock = document.querySelector('.map__pins');
 
 // MODULE4-TASK1
 
-var convertToArr = function (someValue) {
-  var someArray = [];
-  someArray = someArray.concat(someValue);
-
-  return someArray;
-};
-
-var disableElem = function (nodeOrNodeList) {
-  convertToArr(nodeOrNodeList);
-
-  nodeOrNodeList.forEach(function (node) {
-    node.setAttribute('disabled', '');
-  });
-
-  return;
-};
-
-var activateElem = function (nodeOrNodeList) {
-  convertToArr(nodeOrNodeList);
-
-  nodeOrNodeList.forEach(function (node) {
-    node.removeAttribute('disabled', '');
-  });
-
-  return;
-};
-
 var activateMap = function (condition) {
   if (condition === true) {
     mapBlock.classList.remove('map--faded');
@@ -290,36 +187,22 @@ var activateMap = function (condition) {
 
 var activateForm = function (condition) {
   if (condition === true) {
-    activateElem(fieldsets);
+    window.util.activateElem(fieldsets);
     notice.querySelector('.ad-form').classList.remove('ad-form--disabled');
   } else {
-    disableElem(fieldsets);
+    window.util.disableElem(fieldsets);
     notice.querySelector('.ad-form').classList.add('ad-form--disabled');
   }
-};
-
-var getElemCoordinate = function (elem) {
-  return {
-    left: parseInt(getComputedStyle(elem).left, 10),
-    top: parseInt(getComputedStyle(elem).top, 10)
-  };
-};
-
-var getElemCenter = function (elem) {
-  return {
-    x: Math.ceil(elem.offsetWidth / 2),
-    y: Math.ceil(elem.offsetHeight / 2)
-  };
 };
 
 var getPointerCoordinate = function (isMapActive) {
   return isMapActive === true ?
     {
-      left: getElemCoordinate(Pointer).left + getElemCenter(Pointer).x,
-      top: getElemCoordinate(Pointer).top + Pointer.offsetHeight + POINTER_ARROW_HEIGHT
+      left: window.util.getElemCoordinate(Pointer).left + window.util.getElemCenter(Pointer).x,
+      top: window.util.getElemCoordinate(Pointer).top + Pointer.offsetHeight + POINTER_ARROW_HEIGHT
     } : {
-      left: getElemCoordinate(Pointer).left + getElemCenter(Pointer).x,
-      top: getElemCoordinate(Pointer).top + getElemCenter(Pointer).y
+      left: window.util.getElemCoordinate(Pointer).left + window.util.getElemCenter(Pointer).x,
+      top: window.util.getElemCoordinate(Pointer).top + window.util.getElemCenter(Pointer).y
     };
 };
 
@@ -338,34 +221,32 @@ var roomsField = notice.querySelector('#room_number');
 var capacityField = notice.querySelector('#capacity');
 var formSubmitButton = notice.querySelector('.ad-form__submit');
 
-disableElem(fieldsets);
+window.util.disableElem(fieldsets);
 setAddress(false);
 
 var onPinClick = function (evt) {
   var coordinates = {};
 
   if (evt.target.parentNode.hasAttribute('style')) {
-    coordinates = getElemCoordinate(evt.target.parentNode);
+    coordinates = window.util.getElemCoordinate(evt.target.parentNode);
   } else if (evt.target.hasAttribute('style')) {
-    coordinates = getElemCoordinate(evt.target);
+    coordinates = window.util.getElemCoordinate(evt.target);
   }
 
   return coordinates;
 };
 
 var onPopupEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closeMapPopup();
-  }
+  window.util.isEscEvent(evt, closeMapPopup);
 };
 
 var closeMapPopup = function () {
-  deleteChilds('.map__card', mapBlock);
+  window.util.deleteChilds('.map__card', mapBlock);
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
 var createCurrentPopup = function (evt) {
-  deleteChilds('.map__card', mapBlock);
+  window.util.deleteChilds('.map__card', mapBlock);
 
   for (var i = 0; i < advertisements.length; i++) {
     if (onPinClick(evt).left === advertisements[i].location.x) {
@@ -459,7 +340,7 @@ var onPointerCatched = function (evt) {
   var onPointerWasMoved = function () {
     setAddress(true);
     activateForm(true);
-    addElem(deleteChilds('button[type=button]', mapPinsBlock), createPins(advertisements));
+    window.util.addElem(window.util.deleteChilds('button[type=button]', mapPinsBlock), createPins(advertisements));
 
     Pointer.removeEventListener('mouseleave', onPointerWasMoved);
     Pointer.removeEventListener('mousemove', onPointerMove);
