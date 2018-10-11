e344466010a8e0532987b6dc744e1d226ea64591
Andrey Koreykin <andrey.koreykin@gmail.com>
2018-10-11 05:30:31 +0300

    теперь Ñданные приходят с сервера

---
+++ js/backend.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// backend.js
+(function () {
+  var DATA = 'https://js.dump.academy/keksobooking/data';
+  var URL = 'https://js.dump.academy/keksobooking';
+
+  window.backend = {
+    get: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+          // console.log(xhr.status);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+
+      xhr.open('GET', DATA);
+      xhr.send();
+
+    },
+    post: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+
+          window.form.onSuccess();
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    }
+  };
+
+})();
